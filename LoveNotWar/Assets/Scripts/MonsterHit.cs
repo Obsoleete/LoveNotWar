fa42ae7fa07643ddc5d740e0f3ce129ed311d587
Hamza Khan <hz.khan@mail.utoronto.ca>
2021-01-16 13:44:11 -0500

    Added monster sprite with hit collision - Hamza Khan

---
+++ LoveNotWar/Assets/Scripts/MonsterHit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Detects if the Monster is hit by the heart bullet
+public class MonsterHit : MonoBehaviour
+{
+
+    public SpawnMonster SpawnMonster;
+
+    // Starts out by detecting the Spawn Monster Object
+    void Start()
+    {
+        SpawnMonster = FindObjectOfType<SpawnMonster>();
+    }
+
+    private void OnCollisionEnter(Collision collision) 
+    {
+        if(collision.collider.tag == "Heart")
+        {
+            SpawnMonster.IncreaseHealth();
+        }
+    }
+}
